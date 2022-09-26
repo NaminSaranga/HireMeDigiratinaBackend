6b1323c3da9cb5eb396fb6059248f9cfe6cf8c83
NaminSaranga <naminsaranga54@gmail.com>
2022-09-26 10:32:24 +0530

    Employee Controller Updated

--- springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,12 @@ public class EmployeeController {
 	public List<EmployeeDigiratina> getAllEmployees(){
 		return employeeRepository.findAll();
 	}	
+	
+	// create employee
+	
+	@PostMapping("/employees")
+	public EmployeeDigiratina createEmployee(@RequestBody EmployeeDigiratina employeeDigiratina) {
+		return employeeRepository.save(employeeDigiratina);
+	}
 
 }
